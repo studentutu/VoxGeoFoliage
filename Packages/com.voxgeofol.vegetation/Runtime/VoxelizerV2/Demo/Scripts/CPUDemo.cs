a36001a41c18512da0193e19aa1bf522e976666a
studentutu <studentutu@gmail.com>
2026-04-01 01:45:51 +0300

    chore: added faster voxelizer

---
+++ Packages/com.voxgeofol.vegetation/Runtime/VoxelizerV2/Demo/Scripts/CPUDemo.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace VoxelSystem.Demo {
+
+    [RequireComponent (typeof(MeshFilter))]
+    public class CPUDemo : MonoBehaviour {
+
+		[SerializeField] protected Mesh mesh;
+        [Tooltip("Smaller means large voxels, 3 is very crud, 8 is minecraft, 15 is half-minecraft, 40 is is good enought for sub-voxels, 120 is almost indistinguishable from mesh (losing holes)")]
+        [SerializeField] protected int resolution = 24;
+        [SerializeField] protected bool useUV = false;
+
+        [SerializeField] private bool Generate = false;
+        
+        private void OnValidate()
+        {
+            if (Generate)
+            {
+                Generate = false;
+                GenerateMesh();
+            }
+        }
+
+        void GenerateMesh () {
+            List<Voxel_t> voxels;
+            float unit;
+            CPUVoxelizer.Voxelize(mesh, resolution, out voxels, out unit);
+
+            var filter = GetComponent<MeshFilter>();
+            filter.sharedMesh = VoxelMesh.Build(voxels.ToArray(), unit, useUV);
+        }
+
+    }
+
+}
+
+
---
+++ Packages/com.voxgeofol.vegetation/Runtime/VoxelizerV2/Scripts/CpuMeshBuildUtility.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace VoxelSystem
+{
+    public static class CpuMeshBuildUtility
+    {
+        /// <summary>
+        ///     CPU based mesh voxelization.
+        ///     Important: not saved anywhere, so if you need persistence, make sure to save it.
+        /// </summary>
+        /// <param name="fromMesh">From mesh.</param>
+        /// <param name="resolution">Smaller resolution means bigger voxels.</param>
+        /// <returns>Voxelized mesh</returns>
+        public static Mesh BuildVoxelizedMesh(Mesh fromMesh, int resolution = 24)
+        {
+            List<Voxel_t> voxels;
+            float unit;
+            CPUVoxelizer.Voxelize(fromMesh, resolution, out voxels, out unit);
+
+            return VoxelMesh.Build(voxels.ToArray(), unit, false);
+        }
+    }
+}
